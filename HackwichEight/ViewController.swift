f504a5351896e625414426db38d272a4049b1f7d
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-03-11 13:23:12 -0800

    In class troubleshooting slider fixed. Manually added line to set slider to 50

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -7,40 +7,58 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var slider: UISlider!
     
-    var currentValue = 0
+    var currentValue = 50
+    
+    var targetValue = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        slider.value = 50
+        
         // Do any additional setup after loading the view.
+        currentValue = Int(slider.value)
+        //Int has an instance method called random
+        targetValue = Int.random(in: 0...100)
+    }
+    
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        
+        print("The value of the slider is: \(slider.value)")
+        currentValue = Int(slider.value)
+    
     }
 
-    @IBAction func sliderHasMoved(_ sender: Any) {
+    //function to start a new round so that we get a new target value
     
-    print("The value of the slider is: \(slider.value) ")
+    @IBOutlet weak var targetLabel: UILabel!
+    func startNewRound(){
+        
+        
+        targetValue = Int.random(in: 0...100)
         currentValue = Int(slider.value)
         
     }
     
-    
-    @IBAction func MyGuessButtonPressed(_ sender: Any) {
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
         
-        let message = "The value is\(currentValue)"
         
-        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        func updateTarget
+        
+        
+        
+        let message = "The value is\(currentValue)" + " \n The Target value is: \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello World", message: message , preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-        
-        
     }
-    
 }
-
