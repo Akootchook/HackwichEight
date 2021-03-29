0fd6408196cdd6a49a8d2e303b7c5f191f4bde21
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-03-28 22:45:46 -0700

    added comments for each section of hackwich eight

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -2,63 +2,55 @@
 //  ViewController.swift
 //  HackwichEight
 //
-//  Created by Berdell Akootchook on 3/9/21.
+//  Created by Marion Ano on 3/9/21.
+//  Copyright © 2021 Marion Ano. All rights reserved.
 //
-
 import UIKit
 
-
 class ViewController: UIViewController {
-
+// viewcontroller
     @IBOutlet weak var slider: UISlider!
-    
-    var currentValue = 50
-    
+    //IBOulet for slider
+    var currentValue = 0
+    //creates var of "currentValue"
     var targetValue = 0
-    
+    //creates var of"targetValue"
     override func viewDidLoad() {
         super.viewDidLoad()
-        slider.value = 50
-        
         // Do any additional setup after loading the view.
         currentValue = Int(slider.value)
-        //Int has an instance method called random
+        //sets number of slider
         targetValue = Int.random(in: 0...100)
+       //sets value of slider from 0-100
+   
+        
+        
+            //set image to slider
+        let thumbImageNormal = UIImage(named: "SliderTumb-Normal")
+        slider.normal
+        //sets image of the icon
+        
+        
     }
     
     @IBAction func sliderHasMoved(_ sender: Any) {
-        
+        //when the slider moves then do this
         print("The value of the slider is: \(slider.value)")
         currentValue = Int(slider.value)
-    
+    //shows value of slider
     }
 
-    //function to start a new round so that we get a new target value
-    
-    @IBOutlet weak var targetLabel: UILabel!
-    func startNewRound(){
-        
-        
-        targetValue = Int.random(in: 0...100)
-        currentValue = Int(slider.value)
-        
-    }
-    
     @IBAction func myGuessButtonPressed(_ sender: Any) {
-        
-        
-        func updateTarget
-        
-        
-        
-        let message = "The value is\(currentValue)" + " \n The Target value is: \(targetValue)"
-        
+        //when button is pressed do this
+        let message = "The value is\(currentValue)"
+        //when slider is set the currentValue is xxx
         let alert = UIAlertController(title: "Hello World", message: message , preferredStyle: .alert)
-        
+        //message or Hello World
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
-        
+        //displays "awesome"
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
     }
 }
+//
